fc9b28c3068e755123ed2549e79d49d1c0377f69
Tatone26 <anatolesainero@gmail.com>
2022-09-21 15:02:24 +0200

    juste une sauvegard

--- chess.py
+++ chess.py
@@ -14,17 +14,11 @@ green = color(0, 82, 62)
 -> faire confiance aux joueurs pour le mat ? 
 """
 
-board = [[None]*8]*8
-print(len(board[7]))
+board = []
+
 
 def draw_case(pos):
     fill_rect(pos[0] * 22 + 72, pos[1] * 22 + 22, 22, 22, get_box_color(pos))
-
-
-def draw_piece(pos, n):
-    """n being the type of piece, as the index in this list : [pawn, rook, knight, bishop, queen, king]"""
-    lp = ["P", "R", "N", "B", "Q", "K"]
-    draw_string(lp[n], pos[0] * 22 + 78, pos[1] * 22 + 24, black, get_box_color(pos))
 
 
 def get_box_color(pos):
@@ -34,23 +28,59 @@ def get_box_color(pos):
         return 175, 175, 175
 
 
-def chess():
+def draw_piece(pos, n):
+    """n being the type and color of the piece, as the index in this list : [pawn, rook, knight, bishop, queen, king]
+    and 0 for white and 1 for black (everything in a tuple)"""
+    lp = ["P", "R", "N", "B", "Q", "K"]
+    draw_string(lp[n[0]], pos[0] * 22 + 78, pos[1] * 22 + 24, black if n[1] else green, get_box_color(pos))
+
+
+def move_piece(oldpos, newpos):
+    global board
+    piece = board[oldpos[0]][oldpos[1]]
+    board[oldpos[0]][oldpos[1]] = None
+    print(piece)
+    draw_case(oldpos)
+    board[newpos[0]][newpos[1]] = piece
+    draw_piece(newpos, piece)
+
+
+def create_initial_board():
+    global board
+    for i in range(8):
+        board.append([])
+        for j in range(8):
+            board[i].append(None)
     fill_rect(0, 0, 320, 222, green)
     fill_rect(70, 20, 180, 180, black)
     for i in range(8):
-        draw_string(str(i+1), i*22 + 78, 2, black, green)
-        draw_string(str(i+1), i*22 + 78, 202, black, green)
+        draw_string(str(i + 1), i * 22 + 78, 2, black, green)
+        draw_string(str(i + 1), i * 22 + 78, 202, black, green)
         for j in range(8):
             draw_case((i, j))
     ltrs = ["A", "B", "C", "D", "E", "F", "G", "H"]
     for i in range(8):
-        draw_string(ltrs[i], 56, i*22+24, black, green)
+        draw_string(ltrs[i], 56, i * 22 + 24, black, green)
+        draw_string(ltrs[i], 254, i * 22 + 24, black, green)
 
     start_pos = [[1, 2, 3, 4, 5, 3, 2, 1], [0, 0, 0, 0, 0, 0, 0, 0]]
     for i in range(len(start_pos)):
         for j in range(len(start_pos[i])):
-            draw_piece((i, j), start_pos[i][j])
-            draw_piece((7-i, j), start_pos[i][j])
+            piece1 = (start_pos[i][j], 0)
+            piece2 = (start_pos[i][j], 1)
+            draw_piece((i, j), piece1)
+            draw_piece((7 - i, j), piece2)
+            board[i][j] = piece1
+            board[7-i][j] = piece2
+
+
+def selection(pos):
+    pass
+
+
+def chess():
+    create_initial_board()
+    move_piece((1, 2), (3, 2))
 
 
 def menu_chess():
