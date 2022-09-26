f83d1947db03d7da8fd6129a919452221eee0a89
Tatone26 <anatolesainero@gmail.com>
2022-09-26 12:24:01 +0200

    progress2

--- chess.py
+++ chess.py
@@ -1,6 +1,6 @@
 from kandinsky import *
 from ion import *
-from menu import menu
+from menu import menu, fill_screen
 from time import *
 
 white = color(255, 255, 255)
@@ -17,7 +17,7 @@ def get_box_color(pos):
         return 175, 175, 175
 
 
-def auto_draw_box(pos, c=None):
+def draw_box(pos, c=None):
     fill_rect(pos[0] * 22 + 72, pos[1] * 22 + 22, 22, 22, get_box_color(pos) if c is None else c)
     n = board[pos[0]][pos[1]]
     if n is not None:
@@ -31,8 +31,8 @@ def move_piece(oldpos, newpos):
     piece = board[oldpos[0]][oldpos[1]]
     board[oldpos[0]][oldpos[1]] = None
     board[newpos[0]][newpos[1]] = piece
-    auto_draw_box(oldpos)
-    auto_draw_box(newpos)
+    draw_box(oldpos)
+    draw_box(newpos)
 
 
 def create_initial_board():
@@ -54,29 +54,31 @@ def create_initial_board():
             board[7 - i][j] = piece2
 
     # Drawing board
-    fill_rect(0, 0, 320, 222, green)
+    fill_screen(green)
     fill_rect(70, 20, 180, 180, black)
+    fill_rect(0, 0, 10, 222, white)
+    fill_rect(310, 0, 320, 222, black)
     for i in range(8):
         draw_string(str(i + 1), i * 22 + 78, 2, black, green)
         draw_string(str(i + 1), i * 22 + 78, 202, black, green)
         for j in range(8):
-            auto_draw_box((i, j))
+            draw_box((i, j))
     letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
     for i in range(8):
         draw_string(letters[i], 56, i * 22 + 24, black, green)
         draw_string(letters[i], 254, i * 22 + 24, black, green)
 
 
-def selection(init_pos):
+def selection(init_pos, player):
     pos = init_pos.copy()
     moves = []
     selected_piece = None
 
     def draw_selection():
-        auto_draw_box(pos, c=(255, 0, 0))
+        draw_box(pos, c=(255, 0, 0))
 
     def clear_selection():
-        auto_draw_box(pos, c=(0, 255, 0) if pos in moves else None)
+        draw_box(pos, c=(0, 255, 0) if pos in moves else None)
 
     draw_selection()
     while True:
@@ -91,22 +93,25 @@ def selection(init_pos):
             mvt = (0, -1)
         elif keydown(KEY_OK):
             for p in moves:
-                auto_draw_box(p)
-            if pos in moves and selected_piece is not None:
+                draw_box(p)
+            if pos in moves and selected_piece is not None and board[selected_piece[0]][selected_piece[1]][1] == player:
+                if board[pos[0]][pos[1]] is not None and board[pos[0]][pos[1]][1] == 5:
+                    draw_string("VICTOIRE", 0, 0)
                 move_piece(selected_piece, pos)
-                moves.clear()
-                selected_piece = None
+                # MAX DE TRUCS A VERIFIER ICI
+                return pos
             else:
                 if selected_piece != pos and board[pos[0]][pos[1]] is not None:
                     selected_piece = pos.copy()
                     moves = get_possible_moves(pos)
                     for i in moves:
-                        auto_draw_box(i, c=(0, 255, 0))
+                        draw_box(i, c=(0, 255, 0))
                 else:
                     selected_piece = None
                     moves.clear()
+                draw_selection()
             sleep(0.1)
-        if mvt != (0, 0) and 0 <= pos[0]+mvt[0] <= 7 and 0 <= pos[1]+mvt[1] <= 7:
+        if mvt != (0, 0) and 0 <= pos[0] + mvt[0] <= 7 and 0 <= pos[1] + mvt[1] <= 7:
             clear_selection()
             pos[0] += mvt[0]
             pos[1] += mvt[1]
@@ -115,8 +120,6 @@ def selection(init_pos):
 
 
 def get_possible_moves(pos):
-    """Renvoie les mouvements possibles de la pièce à cette position.
-    Renvoie une liste vide si il n'y a pas de pièce."""
     p = board[pos[0]][pos[1]]
     if p is None:
         return []
@@ -128,9 +131,8 @@ def get_possible_moves(pos):
                 if pos[0] == 1 + 5 * p[1] and board[pos[0] + 2 - 4 * p[1]][pos[1]] is None:  # DOUBLE SI PAS BOUGE
                     moves.append([pos[0] + 2 - 4 * p[1], pos[1]])
             for i in [1, -1]:  # BOUFFER LES AUTRES
-                if board[pos[0] + 1 - 2 * p[1]][pos[1] + i] is not None and board[pos[0] + 1 - 2 * p[1]][pos[1] + i][
-                    1] != \
-                        p[1]:
+                if 0<= pos[1]+i <= 7 and board[pos[0] + 1 - 2 * p[1]][pos[1] + i] is not None and \
+                        board[pos[0] + 1 - 2 * p[1]][pos[1] + i][1] != p[1]:
                     moves.append([pos[0] + 1 - 2 * p[1], pos[1] + i])
         d = []
         if p[0] == 1 or p[0] == 4:  # LIGNES DROITES
@@ -145,11 +147,11 @@ def get_possible_moves(pos):
                 i += e[0]
                 j += e[1]
         d = []
-        if p[0] == 2:  # CHEVAL
+        if p[0] == 2:
             d = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, -2), (-1, -2), (1, 2), (-1, -2)]
         if p[0] == 5:
             d = [(1, 0), (1, 1), (0, 1), (-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1)]
-        for i in d:
+        for i in d:  # CHECK LIMITE (SEULEMENT MVT PRECISES DANS d)
             if 0 <= pos[0] + i[0] <= 7 and 0 <= pos[1] + i[1] <= 7:
                 if board[pos[0] + i[0]][pos[1] + i[1]] is None or board[pos[0] + i[0]][pos[1] + i[1]][1] != p[1]:
                     moves.append([pos[0] + i[0], pos[1] + i[1]])
@@ -157,15 +159,20 @@ def get_possible_moves(pos):
 
 
 def chess():
+    def draw_turn(p):
+        fill_rect(15 + 265 * p, 5, 25, 10, (255, 0, 0))
+        fill_rect(15 + 265 * (0 ** p), 5, 25, 10, green)
+
     create_initial_board()
-    move_piece((1, 2), (5, 3))
-    move_piece((1, 0), (5, 2))
-    move_piece((6, 0), (5, 1))
-    move_piece((1, 3), (2, 1))
-    move_piece((0, 3), (1, 2))
-    move_piece((6, 6), (4, 6))
-    move_piece((6, 1), (4, 1))
-    selection([0, 0])
+    pos1 = [1, 3]
+    pos2 = [6, 4]
+    while True:
+        draw_turn(0)
+        pos1 = selection(pos1, 0)
+        sleep(0.2)
+        draw_turn(1)
+        pos2 = selection(pos2, 1)
+        sleep(0.2)
 
 
 def menu_chess():
