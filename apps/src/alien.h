4d8bede8bf9e163bdd2dd18d62e3d5cced51ffcc
Tatone26 <anatolesainero@gmail.com>
2022-10-03 11:41:10 +0200

    new apps

---
+++ apps/src/alien.h
@@ -0,0 +1,29 @@
+#ifndef ALIEN_H
+#define ALIEN_H
+
+#include "eadkpp.h"
+#include "spaceship.h"
+
+class Alien {
+public:
+  Alien(int x = -1);
+  int x() const { return m_x; }
+  int y() const { return m_y; }
+  void step();
+  bool tryToHit(Spaceship * s);
+  bool isGhost() const { return m_x == -1; }
+  void killed();
+  static constexpr int k_stepPeriod = 10;
+  static constexpr int k_materializationPeriod = 20;
+  static constexpr int k_width = 15;
+  static constexpr int k_height = 18;
+private:
+  void draw(const EADK::Display::Color c) const;
+  void hide() const;
+  void ghostify() { m_x = -1; }
+  static constexpr int k_step = 10;
+  int m_x;
+  int m_y;
+};
+
+#endif
---
+++ apps/src/rocket.h
@@ -0,0 +1,27 @@
+#ifndef ROCKET_H
+#define ROCKET_H
+
+#include "display.h"
+#include "eadkpp.h"
+
+class Alien;
+
+class Rocket {
+public:
+  Rocket() : m_x(-1), m_y(-1) {}
+  int x() const { return m_x; }
+  int y() const { return m_y; }
+  void setLocation(int x, int y);
+  void forward();
+  bool tryToKill(Alien * a);
+  bool off() const { return m_y < 0; }
+  static constexpr int k_period = 5;
+private:
+  static constexpr int k_length = 13;
+  void draw(const EADK::Display::Color c) const;
+  void switchOff() { m_y = -1; }
+  int m_x;
+  int m_y;
+};
+
+#endif
