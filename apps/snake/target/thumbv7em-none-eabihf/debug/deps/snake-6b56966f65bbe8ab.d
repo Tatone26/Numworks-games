0ce7d6c9df783155e1fcdaaf2f0a4b9a24d5c0ac
Tatone26 <anatolesainero@gmail.com>
2022-11-06 19:44:53 +0100

    rust update

---
+++ apps/snake/target/thumbv7em-none-eabihf/debug/deps/snake-6b56966f65bbe8ab.d
@@ -0,0 +1,10 @@
+C:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\snake\target\thumbv7em-none-eabihf\debug\deps\snake-6b56966f65bbe8ab: src\main.rs src\eadk.rs src\menu.rs src\utils.rs src\game.rs src\../target/icon.nwi
+
+C:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\snake\target\thumbv7em-none-eabihf\debug\deps\snake-6b56966f65bbe8ab.d: src\main.rs src\eadk.rs src\menu.rs src\utils.rs src\game.rs src\../target/icon.nwi
+
+src\main.rs:
+src\eadk.rs:
+src\menu.rs:
+src\utils.rs:
+src\game.rs:
+src\../target/icon.nwi:
---
+++ apps/snake/target/thumbv7em-none-eabihf/release/deps/snake-f4d5a42769619720.d
@@ -0,0 +1,10 @@
+C:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\snake\target\thumbv7em-none-eabihf\release\deps\snake-f4d5a42769619720: src\main.rs src\eadk.rs src\menu.rs src\utils.rs src\game.rs src\../target/icon.nwi
+
+C:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\snake\target\thumbv7em-none-eabihf\release\deps\snake-f4d5a42769619720.d: src\main.rs src\eadk.rs src\menu.rs src\utils.rs src\game.rs src\../target/icon.nwi
+
+src\main.rs:
+src\eadk.rs:
+src\menu.rs:
+src\utils.rs:
+src\game.rs:
+src\../target/icon.nwi:
