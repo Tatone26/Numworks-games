16bb0223dabaa9a15193c8460201860135ed9dc6
Tatone26 <anatolesainero@gmail.com>
2023-01-22 19:31:30 +0100

    test

---
+++ apps/puissance4/target/thumbv7em-none-eabihf/debug/deps/connect_four-e1c18378f748b98b.d
@@ -0,0 +1,10 @@
+c:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\puissance4\target\thumbv7em-none-eabihf\debug\deps\connect_four-e1c18378f748b98b.rmeta: src\main.rs src\game_p4.rs src\ui_p4.rs src\./data/p4_controls.txt src\./data/coins.ppm src\../target/icon.nwi
+
+c:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\puissance4\target\thumbv7em-none-eabihf\debug\deps\connect_four-e1c18378f748b98b.d: src\main.rs src\game_p4.rs src\ui_p4.rs src\./data/p4_controls.txt src\./data/coins.ppm src\../target/icon.nwi
+
+src\main.rs:
+src\game_p4.rs:
+src\ui_p4.rs:
+src\./data/p4_controls.txt:
+src\./data/coins.ppm:
+src\../target/icon.nwi:
---
+++ apps/snake/target/thumbv7em-none-eabihf/debug/deps/snake-ded07d80b0b34107.d
@@ -0,0 +1,10 @@
+c:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\snake\target\thumbv7em-none-eabihf\debug\deps\snake-ded07d80b0b34107.rmeta: src\main.rs src\snake_ui.rs src\game_snake.rs src\./data/tiles.ppm src\./data/snake_controls.txt src\../target/icon.nwi
+
+c:\Users\Anatole\OneDrive\Documents\Prog\python\calculatrice_numworks\Numworks\apps\snake\target\thumbv7em-none-eabihf\debug\deps\snake-ded07d80b0b34107.d: src\main.rs src\snake_ui.rs src\game_snake.rs src\./data/tiles.ppm src\./data/snake_controls.txt src\../target/icon.nwi
+
+src\main.rs:
+src\snake_ui.rs:
+src\game_snake.rs:
+src\./data/tiles.ppm:
+src\./data/snake_controls.txt:
+src\../target/icon.nwi:
