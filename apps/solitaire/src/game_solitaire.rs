5c139d8d48f02f53f6bb7864555f92299ae148e0
Tatone26 <anatolesainero@gmail.com>
2023-06-10 18:52:15 +0200

    saving

--- apps/solitaire/src/game_solitaire.rs
+++ apps/solitaire/src/game_solitaire.rs
@@ -1,17 +1,17 @@
 use heapless::Vec;
-use numworks_utils::utils::draw_tile;
-
-
+use numworks_utils::{
+    eadk::{
+        key, keyboard,
+        timing::{self, msleep},
+    },
+    utils::{randint, wait_for_no_keydown},
+};
 
 use crate::{
-    eadk::{
-        display::{self, push_rect_uniform},
-        key, keyboard, Color, Point, Rect,
-    },
+    eadk::{display::push_rect_uniform, Color, Rect},
     menu::{menu, MyOption, OptionType},
-    utils::{
-        draw_string_cfg, fill_screen, wait_for_no_keydown, ColorConfig, Tileset,
-    }, ui_solitaire::ui_test,
+    ui_solitaire::{draw_selection, draw_stock, ui_test},
+    utils::{fill_screen, ColorConfig},
 };
 
 // This dictates the principal colors that will be used
@@ -76,28 +76,214 @@ pub fn start() {
     }
 }
 
+/// The number associated with each values is the position in the tileset.
+#[derive(Debug, Copy, Clone)]
+pub enum Suit {
+    Club = 2,
+    Diamond = 3,
+    Heart = 1,
+    Spade = 0,
+}
 
+#[derive(Debug, Copy, Clone)]
+pub struct Card {
+    pub suit: Suit,
+    pub number: u8,
+    pub visible: bool,
+}
+
+pub struct Table {
+    pub tableau: [Vec<Card, 52>; 7],
+    pub fondations: [Vec<Card, 13>; 4],
+    pub stock: Vec<Card, 52>,
+    pub stock_iter: usize,
+}
+
+impl Table {
+    pub fn empty() -> Self {
+        let table = Table {
+            tableau: [
+                Vec::new(),
+                Vec::new(),
+                Vec::new(),
+                Vec::new(),
+                Vec::new(),
+                Vec::new(),
+                Vec::new(),
+            ],
+            fondations: [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
+            stock: Vec::new(),
+            stock_iter: 0,
+        };
+        return table;
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum CursorPos {
+    Tableau(u8),
+    Fondations(u8),
+    Stock(u8),
+}
+
+pub trait FromValue {
+    fn from_value(value: &CursorPos) -> Self;
+}
+
+impl FromValue for u8 {
+    fn from_value(value: &CursorPos) -> Self {
+        match value {
+            CursorPos::Tableau(b) => *b,
+            CursorPos::Fondations(b) => *b,
+            CursorPos::Stock(b) => *b,
+        }
+    }
+}
+
+fn new_cursor_pos_value(cursor_pos: CursorPos, new_value: u8) -> CursorPos {
+    match cursor_pos {
+        CursorPos::Tableau(_) => return CursorPos::Tableau(new_value),
+        CursorPos::Fondations(_) => return CursorPos::Fondations(new_value),
+        CursorPos::Stock(_) => return CursorPos::Stock(new_value),
+    }
+}
+
+const REPETITION_SPEED: u16 = 200;
 
 /// The entire game is here.
 pub fn game(_exemple: bool) -> u8 {
     {
-        fill_screen(Color::WHITE);
-        draw_string_cfg(
-            "Push <OK> for some magic ! <Back> to quit back to menu.\0",
-            Point::new(0, 0),
-            false,
-            &COLOR_CONFIG,
-            true,
-        );
+        let mut table = create_table();
+        ui_test(&table);
+        let mut cursor_pos = CursorPos::Tableau(3);
+        draw_selection(cursor_pos, true, &table);
+        wait_for_no_keydown();
+        let mut last_action: u64 = timing::millis();
+        let mut last_action_key: u32 = key::ALPHA;
         loop {
             let keyboard_state = keyboard::scan();
-            if keyboard_state.key_down(key::OK) {
-                ui_test();
-            } else if keyboard_state.key_down(key::BACK) {
-                break;
+            if (keyboard_state.key_down(key::DOWN)
+                || keyboard_state.key_down(key::UP)
+                || keyboard_state.key_down(key::LEFT)
+                || keyboard_state.key_down(key::RIGHT))
+                && (timing::millis() >= (last_action + REPETITION_SPEED as u64))
+            {
+                // Move the cursor accordingly, draw selections etc
+                let old_pos = cursor_pos;
+                let value = u8::from_value(&cursor_pos);
+                if keyboard_state.key_down(key::LEFT) {
+                    last_action_key = key::LEFT;
+                    if value > 0 {
+                        cursor_pos = new_cursor_pos_value(cursor_pos, value - 1);
+                    } else if let CursorPos::Stock(_) = cursor_pos {
+                        cursor_pos = CursorPos::Fondations(3);
+                    }
+                } else if keyboard_state.key_down(key::RIGHT) {
+                    last_action_key = key::RIGHT;
+                    match cursor_pos {
+                        CursorPos::Tableau(_) => {
+                            if value < 6 {
+                                cursor_pos = new_cursor_pos_value(cursor_pos, value + 1)
+                            }
+                        }
+                        CursorPos::Fondations(_) => {
+                            if value < 3 {
+                                cursor_pos = new_cursor_pos_value(cursor_pos, value + 1)
+                            } else {
+                                cursor_pos = CursorPos::Stock(0)
+                            }
+                        }
+                        CursorPos::Stock(_) => {
+                            if value < 1 {
+                                cursor_pos = new_cursor_pos_value(cursor_pos, value + 1)
+                            }
+                        }
+                    }
+                } else if keyboard_state.key_down(key::UP) {
+                    if let CursorPos::Tableau(_) = cursor_pos {
+                        if value <= 3 {
+                            cursor_pos = CursorPos::Fondations(value);
+                        } else if value == 6 {
+                            cursor_pos = CursorPos::Stock(1);
+                        } else {
+                            cursor_pos = CursorPos::Stock(0);
+                        }
+                    }
+                } else if keyboard_state.key_down(key::DOWN) {
+                    match cursor_pos {
+                        CursorPos::Tableau(_) => (),
+                        CursorPos::Fondations(_) => cursor_pos = CursorPos::Tableau(value),
+                        CursorPos::Stock(_) => {
+                            if value == 0 {
+                                cursor_pos = CursorPos::Tableau(4);
+                            } else {
+                                cursor_pos = CursorPos::Tableau(6);
+                            }
+                        }
+                    }
+                }
+                if old_pos != cursor_pos {
+                    draw_selection(old_pos, false, &table);
+                    draw_selection(cursor_pos, true, &table);
+                }
+                last_action = timing::millis();
+            } else if keyboard_state.key_down(key::EXE)
+                && timing::millis() >= (last_action + REPETITION_SPEED as u64)
+            {
+                if table.stock_iter >= table.stock.len() {
+                    table.stock_iter = 0;
+                } else {
+                    table.stock_iter += 1;
+                }
+                draw_stock(&table.stock, table.stock_iter);
+                last_action_key = key::EXE;
+                last_action = timing::millis();
+            } else if keyboard_state.key_down(key::OK) {
+                todo!();
+            } else if !keyboard_state.key_down(last_action_key) {
+                last_action = timing::millis() - REPETITION_SPEED as u64;
+            }
+        }
+        return 1;
+    }
+}
+
+/// Shuffles in-place the deck, with Fisher-Yates algorithm.
+fn shuffle_deck(deck: &mut Vec<Card, 52>) {
+    for i in (1..52).rev() {
+        let choice = randint(0, i);
+        deck.swap(choice as usize, i as usize);
+    }
+}
+
+fn create_table() -> Table {
+    let mut deck = Vec::<Card, 52>::new();
+    let suit_list = [Suit::Club, Suit::Diamond, Suit::Heart, Suit::Spade];
+    for i in 0..4 {
+        for e in 0..13 {
+            let test = deck.push(Card {
+                suit: suit_list[i],
+                number: e,
+                visible: false,
+            });
+            if test.is_err() {
+                fill_screen(Color::RED);
+                loop {}
             }
         }
     }
-    fill_screen(Color::GREEN);
-    return 1;
+    shuffle_deck(&mut deck);
+    let mut table = Table::empty();
+    for i in 0..7 {
+        for _ in 0..(i + 1) {
+            let toadd = deck.pop();
+            match toadd {
+                Some(_) => unsafe { table.tableau[i].push_unchecked(toadd.unwrap()) },
+                None => panic!(),
+            };
+        }
+        table.tableau[i].last_mut().unwrap().visible = true;
+    }
+    table.stock = deck;
+    return table;
 }
--- apps/solitaire/src/ui_solitaire.rs
+++ apps/solitaire/src/ui_solitaire.rs
@@ -1,4 +1,16 @@
-use numworks_utils::{utils::{Tileset, wait_for_no_keydown, fill_screen, draw_tile}, eadk::{Color, display, Point}};
+use heapless::Vec;
+use numworks_utils::{
+    eadk::{
+        display::{self, draw_string, push_rect_uniform, SCREEN_WIDTH},
+        timing::msleep,
+        Color, Point, Rect,
+    },
+    utils::{
+        draw_tile, fill_screen, wait_for_no_keydown, Tileset, LARGE_CHAR_HEIGHT, LARGE_CHAR_WIDTH,
+    },
+};
+
+use crate::game_solitaire::{Card, CursorPos, Table};
 
 /// Images work really well with square tiles. You can still draw other images, but it is less good.
 static TILESET: Tileset = Tileset {
@@ -7,14 +19,235 @@ static TILESET: Tileset = Tileset {
 };
 const PIXELS: usize = { 35 * 35 } as usize;
 
-pub fn ui_test() {
-    wait_for_no_keydown();
-    fill_screen(Color::from_rgb888(1, 115, 55));
-    display::wait_for_vblank();
-    for i in 0..5 {
-        draw_tile::<PIXELS>(&TILESET, Point::new(i * 37, 0), Point::new(i, 0), 1, false);
-        draw_tile::<PIXELS>(&TILESET, Point::new(i * 37, 35), Point::new(i, 1), 1, true);
+const NAMES_LIST: [&'static str; 13] = [
+    "A\0", "2\0", "3\0", "4\0", "5\0", "6\0", "7\0", "8\0", "9\0", "10\0", "J\0", "Q\0", "K\0",
+];
+
+const BACKGROUND_COLOR: Color = Color::from_rgb888(1, 115, 55);
+const CARD_HEIGHT: u16 = 56;
+const CARD_WIDTH: u16 = 35;
+const HIDDEN_CARD_TILE: u16 = 4;
+
+fn draw_card(card: &Card, pos: Point) {
+    if card.visible {
+        draw_tile::<PIXELS>(&TILESET, pos, Point::new(card.suit as u16, 0), 1, false);
+        draw_tile::<PIXELS>(
+            &TILESET,
+            Point::new(pos.x, pos.y + TILESET.tile_size),
+            Point::new(card.suit as u16, 1),
+            1,
+            true,
+        );
+        draw_string(
+            NAMES_LIST[card.number as usize],
+            Point::new(pos.x + 3, pos.y + 2),
+            true,
+            Color::BLACK,
+            Color::WHITE,
+        );
+        draw_string(
+            NAMES_LIST[card.number as usize],
+            Point::new(
+                pos.x + CARD_WIDTH - 4 - {
+                    if card.number == 9 {
+                        2 * LARGE_CHAR_WIDTH
+                    } else {
+                        LARGE_CHAR_WIDTH
+                    }
+                },
+                pos.y + CARD_HEIGHT - 4 - LARGE_CHAR_HEIGHT,
+            ),
+            true,
+            Color::BLACK,
+            Color::WHITE,
+        );
+    } else {
+        draw_tile::<PIXELS>(&TILESET, pos, Point::new(HIDDEN_CARD_TILE, 0), 1, false);
+        draw_tile::<PIXELS>(
+            &TILESET,
+            Point::new(pos.x, pos.y + TILESET.tile_size),
+            Point::new(HIDDEN_CARD_TILE, 1),
+            1,
+            true,
+        );
     }
-    draw_tile::<PIXELS>(&TILESET, Point::new(0, 0), Point::new(3, 2), 1, true);
-    draw_tile::<PIXELS>(&TILESET, Point::new(0, 35), Point::new(3, 3), 1, true);
+}
+
+fn get_pos_from_cursor_pos(cursor_pos: CursorPos, table: &Table) -> Point {
+    match cursor_pos {
+        CursorPos::Tableau(i) => {
+            return Point::new(20 + (i as u16) * (CARD_WIDTH + 6), CARD_HEIGHT
+            + 25
+            + (i as u16) * {
+                if table.tableau[i as usize].len() <= 7 {
+                    LARGE_CHAR_HEIGHT
+                } else {
+                    LARGE_CHAR_HEIGHT / 2
+                }
+            })
+        }
+        CursorPos::Fondations(i) => return Point::new(20 + (i as u16) * (CARD_WIDTH + 6), 10),
+        CursorPos::Stock(i) => {
+            if i == 1 {
+                return Point::new(SCREEN_WIDTH - CARD_WIDTH - 21, 10);
+            } else {
+                let mut o = 0;
+                if table.stock_iter == 2 {
+                    o = 1;
+                } else if table.stock_iter >= 2 {
+                    o = 2;
+                }
+                return Point::new(SCREEN_WIDTH - 3 * CARD_WIDTH - 21 + o * CARD_WIDTH / 2, 10);
+            }
+        }
+    };
+}
+
+pub fn draw_selection(cursor_pos: CursorPos, selected: bool, table: &Table) {
+    let pos = get_pos_from_cursor_pos(cursor_pos, table);
+    display::wait_for_vblank();
+    let empty: bool = {
+        match cursor_pos {
+            CursorPos::Tableau(b) => table.tableau[b as usize].is_empty(),
+            CursorPos::Fondations(b) => table.fondations[b as usize].is_empty(),
+            CursorPos::Stock(b) => {
+                (b == 0 && table.stock_iter <= 0)
+                    || (b == 1 && table.stock_iter >= table.stock.len())
+            }
+        }
+    };
+    if !selected && empty {
+        draw_empty_place(pos);
+    } else {
+        let tile: u16 = {
+            if selected {
+                3
+            } else {
+                2
+            }
+        };
+        draw_tile::<PIXELS>(&TILESET, pos, Point::new(tile, 2), 1, true);
+        draw_tile::<PIXELS>(
+            &TILESET,
+            Point::new(pos.x, pos.y + TILESET.tile_size),
+            Point::new(tile, 3),
+            1,
+            true,
+        );
+    }
+}
+
+fn draw_empty_place(pos: Point) {
+    draw_tile::<PIXELS>(&TILESET, pos, Point::new(0, 2), 1, true);
+    draw_tile::<PIXELS>(
+        &TILESET,
+        Point::new(pos.x, pos.y + TILESET.tile_size),
+        Point::new(0, 3),
+        1,
+        true,
+    );
+}
+
+fn draw_empty_table() {
+    fill_screen(BACKGROUND_COLOR);
+    let table = Table::empty();
+    for i in 0..4 {
+        draw_selection(CursorPos::Fondations(i), false, &table);
+    }
+    for i in 0..2 {
+        draw_selection(CursorPos::Stock(i), false, &table);
+    }
+    for i in 0..7 {
+        draw_selection(CursorPos::Tableau(i), false, &table);
+    }
+}
+
+fn draw_tableau_stack(stack: &Vec<Card, 52>, number: u16) {
+    if !stack.is_empty() {
+        for (i, c) in stack.iter().enumerate() {
+            draw_card(
+                c,
+                Point::new(
+                    20 + number * (CARD_WIDTH + 6),
+                    CARD_HEIGHT
+                        + 25
+                        + (i as u16) * {
+                            if stack.len() <= 7 {
+                                LARGE_CHAR_HEIGHT
+                            } else {
+                                LARGE_CHAR_HEIGHT / 2
+                            }
+                        },
+                ),
+            );
+        }
+    } else {
+        draw_empty_place(Point::new(20 + number * (CARD_WIDTH + 6), CARD_HEIGHT + 25));
+    }
+}
+
+fn draw_fondations_stack(stack: &Vec<Card, 13>, number: u16) {
+    if !stack.is_empty() {
+        draw_card(
+            stack.last().unwrap(),
+            Point::new(20 + number * (CARD_WIDTH + 6), 10),
+        );
+    } else {
+        draw_empty_place(Point::new(20 + number * (CARD_WIDTH + 6), 10));
+    }
+}
+
+pub fn draw_stock(stack: &Vec<Card, 52>, stock_iter: usize) {
+    display::wait_for_vblank();
+    if !stack.is_empty() {
+        if stock_iter >= stack.len() {
+            draw_empty_place(Point::new(SCREEN_WIDTH - CARD_WIDTH - 21, 10));
+        } else {
+            draw_card(
+                stack.get(stock_iter).unwrap(),
+                Point::new(SCREEN_WIDTH - CARD_WIDTH - 21, 10),
+            );
+        }
+        if stock_iter > 0 {
+            let start: usize = {
+                if (stock_iter as i16 - 3) > 0 {
+                    stock_iter - 3
+                } else {
+                    0
+                }
+            };
+            let mut o: u16 = 0;
+            display::wait_for_vblank();
+            for i in start..stock_iter {
+                let mut card: Card = stack.get(i).unwrap().clone();
+                card.visible = true;
+                draw_card(
+                    &card,
+                    Point::new(SCREEN_WIDTH - 3 * CARD_WIDTH - 21 + o * CARD_WIDTH / 2, 10),
+                );
+                o += 1;
+            }
+        } else {
+            draw_empty_place(Point::new(SCREEN_WIDTH - 3 * CARD_WIDTH - 21, 10));
+        }
+    } else {
+        draw_empty_place(Point::new(SCREEN_WIDTH - CARD_WIDTH - 21, 10));
+        draw_empty_place(Point::new(SCREEN_WIDTH - 3 * CARD_WIDTH - 21, 10))
+    }
+}
+
+fn draw_table(table: &Table) {
+    for i in 0..7 {
+        draw_tableau_stack(&table.tableau[i], i as u16);
+    }
+    for i in 0..4 {
+        draw_fondations_stack(&table.fondations[i], i as u16);
+    }
+    draw_stock(&table.stock, table.stock_iter);
+}
+
+pub fn ui_test(table: &Table) {
+    wait_for_no_keydown();
+    draw_empty_table();
+    draw_table(table);
 }
