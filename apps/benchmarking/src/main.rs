1a506e1b2b5e4f4f65687fd4e334a15518592a6e
Tatone26 <anatolesainero@gmail.com>
2024-01-06 22:35:40 +0100

    new project : benchmarking the capacity of the numworks.

---
+++ apps/benchmarking/src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use numworks_utils::eadk;
+use numworks_utils::menu;
+use numworks_utils::utils;
+
+mod game;
+
+#[used]
+#[link_section = ".rodata.eadk_app_name"]
+pub static EADK_APP_NAME: [u8; 6] = *b"TESTS\0";
+
+#[used]
+#[link_section = ".rodata.eadk_api_level"]
+pub static EADK_APP_API_LEVEL: u32 = 0;
+
+#[used]
+#[link_section = ".rodata.eadk_app_icon"]
+pub static EADK_APP_ICON: [u8; 2286] = *include_bytes!("../target/icon.nwi");
+
+#[no_mangle]
+pub fn main() {
+    game::start();
+}
